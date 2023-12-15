dc2fcf89e55ba97a71069d2753f15e9c96e45bf9
Akinola Adewole <adewoleakinola24@gmail.com>
2023-12-15 17:00:49 +0100

    commit

--- loops/index.js
+++ loops/index.js
@@ -4,4 +4,12 @@
 // console.log('Loop completed');
 
 
-const names = ['shaun', 'mario', 'luigi'];
+const names = ['shaun', 'mario', 'luigi', 'asphalt', 'carl'];
+
+for(let i =0; i< names.length; i++){
+    console.log(i);
+    console.log(names[i]);
+
+    displ.innerHTML = `${names[1]}`
+    
+}
