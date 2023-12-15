280918ee944050592344bc652bf362a764a5bb00
Akinola Adewole <adewoleakinola24@gmail.com>
2023-12-15 11:06:56 +0100

    commit

--- chapter1/script.js
+++ chapter1/script.js
@@ -1,3 +1,14 @@
-let email = 'compasz24@gmail.com';
-let index = email.indexOf('@');
-console.log(index);
+// let email = 'compasz24@gmail.com';
+// let index = email.indexOf('@');
+// console.log(index);
+
+
+// template strings
+const title = 'Best reads of 2023';
+const author = 'Luigi';
+const likes = 30
+
+// Using Template literals
+let result = `The blog called ${title} by ${author} has ${likes} likes`
+console.log(result);
+disp.innerHTML = result;
