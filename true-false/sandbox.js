8c3be30c13adfea79f4b229795b7c0b468e88c88
Akinola Adewole <adewoleakinola24@gmail.com>
2023-12-15 12:15:30 +0100

    commit

--- true-false/sandbox.js
+++ true-false/sandbox.js
@@ -7,3 +7,9 @@ console.log(age > 30);
 console.log(age < 30);
 
 let name = 'Shaun';
+console.log(name === 'shaun');
+console.log(name === 'Shaun');
+
+// In JS, lowercase letters are greater than uppercase letters
+console.log(name < 'shaun');
+console.log(name > 'shaun');
