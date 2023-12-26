50996a1fa39847b5b7d93fa2d1e9c7e09b2a69d8
Akinola Adewole <adewoleakinola24@gmail.com>
2023-12-26 06:58:56 +0100

    JavScript Udacity

--- ReverseFuntion/reverse.js
+++ ReverseFuntion/reverse.js
@@ -8,3 +8,15 @@ const reverseString = (reverseMe) => {
 };
 
 console.log(reverseString('Julia')); // Output: ailuJ
+
+
+
+// Let's break it down:
+
+// The function has one parameter -- a variable named reverseMe.
+// reverseMewill store the argument -- the value of the string that we want the function to operate on.
+// The variable reversed is intialized as an empty string. 
+// It will be used to store the reversed string as as it is being constructed.
+// The function loops through each character the reverseMe string using string indexes, 
+// from the end to the beginning and adds each character to reversed.
+// When the loop is complete, reversed is returned.
