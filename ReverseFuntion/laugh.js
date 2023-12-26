eed258601ac9514bc2f69d90b32d3046a216fe7f
Akinola Adewole <adewoleakinola24@gmail.com>
2023-12-26 07:58:24 +0100

    JavaScript Udacity

---
+++ ReverseFuntion/laugh.js
@@ -0,0 +1,13 @@
+let laughter = "";
+function laugh(num) {
+    for (let i = 0; i < num; i++) {
+        laughter += 'ha';
+    }
+    return laughter + "!";
+}
+
+// test code
+console.log(laugh(0)); // Output: "!"
+console.log(laugh(3)); // Output: "hahaha!"
+console.log(laugh(4)); // Output: "hahahaha!"
+console.og(laugh(8)); // Output: "hahahahahahahaha!"
