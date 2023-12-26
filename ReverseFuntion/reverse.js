6f84a1f0e8547fb0daf2d174f2f6f1a80afa3b09
Akinola Adewole <adewoleakinola24@gmail.com>
2023-12-26 06:49:40 +0100

    JavScript Udacity

--- ReverseFuntion/reverse.js
+++ ReverseFuntion/reverse.js
@@ -1,6 +1,10 @@
-const reverseString=(reverseMe)=>{
-    const reversed = "";
-    for(i = reverseMe.length -1 ; i>=0; i--){
-        reversed += reverseMe[1];
+
+const reverseString = (reverseMe) => {
+    let reversed = ""; // Use let instead of const for reassignment
+    for (let i = reverseMe.length - 1; i >= 0; i--) { // Declare i using let for local scope
+        reversed += reverseMe[i];
     }
-}
+    return reversed;
+};
+
+console.log(reverseString('Julia')); // Output: ailuJ
