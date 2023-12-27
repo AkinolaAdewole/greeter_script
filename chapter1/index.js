e7998b4532a3585dece6b7d8263126cb2eef001c
Akinola Adewole <adewoleakinola24@gmail.com>
2023-12-27 21:28:14 +0100

    JavaScript Udacity

---
+++ chapter1/index.js
@@ -0,0 +1,7 @@
+// Create the udaciFamily array and add names
+let udaciFamily = ["Julia", "James"]; // You can add your name here too
+const yourName = "Your Name"; // Replace "Your Name" with your actual name
+udaciFamily.push(yourName);
+
+// Print udaciFamily to the console
+console.log(udaciFamily);
