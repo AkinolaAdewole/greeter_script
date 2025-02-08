bb76099384b7f60d9675ac5f5a23e98cedab1a71
Akinola Adewole <adewoleakinola24@gmail.com>
2025-02-08 23:19:22 +0100

    JavaScript relearn

--- Array/map.js
+++
@@ -1,7 +0,0 @@
-const bills = [50.23, 19.12, 34.01, 100.11, 12.15, 9.90, 29.11, 12.99, 10.00, 99.22, 102.20, 100.10, 6.77, 2.22];
-
-const totals = bills.map(bill => bill * 0.01); // Calculate 15% tip and add to each bill amount
-const totalz = bills.map(bill => bill);
-
-console.log(totals); // Output the new totals array with 15% tip added
-console.log(totalz);
