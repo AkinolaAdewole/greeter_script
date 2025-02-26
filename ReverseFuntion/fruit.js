b3a6d8b70a41ae6f6c742c1975d9097769c42d22
Akinola Adewole <adewoleakinola24@gmail.com>
2025-02-26 23:39:59 +0100

    Javascript playground

--- ReverseFuntion/fruit.js
+++ ReverseFuntion/fruit.js
@@ -1,7 +1,7 @@
-let fruit = "apple"; 
+let fruit = "apple"; // Declare the variable 'fruit' and initialize it with the value "apple".
 
-if (fruit !== "banana") {
-    let fruit = "banana";
-    console.log(fruit);   
+if (fruit !== "banana") { // Check if 'fruit' is not equal to "banana". Since 'fruit' is "apple", this condition is true.
+    let fruit = "banana"; // Declare a new block-scoped variable 'fruit' and initialize it with the value "banana".
+    console.log(fruit);   // Output the value of this new 'fruit' variable. Since this 'fruit' is scoped inside the if-block, it will output "banana".
 }
-console.log(fruit); 
+console.log(fruit); // Output the value of the outer 'fruit' variable, which is still "apple", since the 'fruit' inside the if-block was block-scoped.
