f913abafe04204f051153c23ce2d7f07819c9ab8
Akinola Adewole <adewoleakinola24@gmail.com>
2025-02-26 23:41:47 +0100

    Javascript playground

--- ReverseFuntion/fruit2.js
+++ ReverseFuntion/fruit2.js
@@ -1,7 +1,7 @@
-var fruit = "apple"; 
+var fruit = "apple"; // Declare the variable 'fruit' using 'var' and initialize it with "apple".
 
-if (fruit !== "banana") {
-    var fruit = "banana";
-    console.log(fruit);   
+if (fruit !== "banana") { // Check if 'fruit' is not equal to "banana". Since 'fruit' is "apple", this condition is true.
+    var fruit = "banana"; // Re-declare 'fruit' using 'var', which is function-scoped or globally-scoped. This changes the value of 'fruit' to "banana" even outside the block.
+    console.log(fruit);   // Output the value of 'fruit'. This will print "banana".
 }
-console.log(fruit);
+console.log(fruit); // Since 'var' is not block-scoped, this will also print "banana", as the value of 'fruit' was changed globally in the 'if' block.
