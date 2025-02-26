cbd2b5ced260d2a7764d3ab9cba01a7cc816d6df
Akinola Adewole <adewoleakinola24@gmail.com>
2025-02-26 23:45:54 +0100

    Javascript playground

--- ReverseFuntion/scope2.js
+++ ReverseFuntion/scope2.js
@@ -1,17 +1,18 @@
 function whereAmIAssessible(a) {
-    if (a) {
-      var x = "x is accessible";
-      let y = 'y is accessible';
-      const z = 'z is accessible';
-      console.log("Inside the if block scope:");
-      console.log(x);
-      console.log(y);
-      console.log(z);
-    }
+  if (a) {
+    var x = "x is accessible";  // Declared with 'var', function-scoped.
+    let y = 'y is accessible';  // Declared with 'let', block-scoped.
+    const z = 'z is accessible'; // Declared with 'const', block-scoped.
+    console.log("Inside the if block scope:");
+    console.log(x);  // Prints: "x is accessible"
+    console.log(y);  // Prints: "y is accessible"
+    console.log(z);  // Prints: "z is accessible"
   }
-  
-  whereAmIAssessible(true);
-  console.log("Outside the function scope:")
-  console.log(x);
-  console.log(y);
-  console.log(z);
+}
+
+whereAmIAssessible(true);
+
+console.log("Outside the function scope:")
+console.log(x); // Will result in an error, because 'x' is function-scoped, not accessible outside the function.
+console.log(y); // Will result in an error, because 'y' is block-scoped to the 'if' statement, not accessible here.
+console.log(z); // Will result in an error, because 'z' is also block-scoped to the 'if' statement.
